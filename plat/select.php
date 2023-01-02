f785c842d407ee22ba2d7bba3eda36a86af6fd8b
andreyaici3 <andreyandri90@gmail.com>
2023-01-02 08:30:52 +0700

    add query keterangan

--- plat/select.php
+++ plat/select.php
@@ -5,7 +5,8 @@ include '../function.php';
 $data["result"] = [
     "status" => 200,
     "msg" => "Success! Data Plat Berhasil di Ambil",
-    "nomorPolisi" => select("nomor_polisi", "")
+    "nomorPolisi" => select("nomor_polisi", ""),
+    "ket" => select("keterangan", ""),
 ];
 
 echo json_encode($data);
