7ec0375acadbde35efa3d03c7d6977d2ef2708b0
andreyaici3 <andreyandri90@gmail.com>
2023-01-03 10:19:21 +0700

    fopen get

--- report/get.php
+++ report/get.php
@@ -2,21 +2,33 @@
 
 include '../function.php';
 
-if ($_POST){
-    $start = $_POST['start'];
-    $end = $_POST['end'];
+// if ($_POST){
+//     $start = $_POST['start'];
+//     $end = $_POST['end'];
 
-    $data["result"] = [
-        "status" => 200,
-        "msg" => "Success! Data Plat Berhasil di Ambil",
-        "report" => report($start, $end)
-    ];
+//     $data["result"] = [
+//         "status" => 200,
+//         "msg" => "Success! Data Plat Berhasil di Ambil",
+//         "report" => report($start, $end)
+//     ];
 
-} else {
-    $data["result"] = [
-        "status" => 403,
-        "msg" => "Error! Data Tidak Boleh Kosong"
-    ];
-}
+// } else {
+//     $data["result"] = [
+//         "status" => 403,
+//         "msg" => "Error! Data Tidak Boleh Kosong"
+//     ];
+// }
 
-echo json_encode($data);
+// echo json_encode($data);
+
+$path = "../file/02.pdf";
+// 
+$myFile = fopen($path, "r");
+
+echo fread($myFile, filesize($path));
+fclose($myFile);
+// 
+
+// $rootDir = ;
+
+// var_dump($rootDir);
